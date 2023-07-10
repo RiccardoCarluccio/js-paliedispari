14df16bf78b123fd9287e4390c039ec09c45dbdf
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-07-10 17:34:50 +0200

    Pali e Dispari completo e funzionante

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,9 @@
 const BTN_INPUT = document.querySelector(".btn-input");
 const USER_INPUT = document.getElementById("user-input");
+const EVEN_ODD_SELECT = document.getElementById("even-odd-select");
+
 let sum;
+let counter = 0;
 
 BTN_INPUT.addEventListener('click', function getData() {
   const USER_NUM = USER_INPUT.value;
@@ -8,6 +11,28 @@ BTN_INPUT.addEventListener('click', function getData() {
   
   //TEST somma
   console.log(`The sum is: ${sum}`);
+
+  //SEZIONE SELECT
+  const userChoice = EVEN_ODD_SELECT.value;
+  if (userChoice === 'Odd') {
+    if (sum % 2 === 0) {
+      console.log('The sum was even. You lost.');
+      counter--;
+    } else {
+      console.log('The sum was odd. You won.');
+      counter++;
+    }
+  } else {
+    if (sum % 2 === 0) {
+      console.log('The sum was even. You won.');
+      counter++;
+    } else {
+      console.log('The sum was odd. You lost.')
+      counter--;
+    }
+  }
+  console.log(`Your win/loss edge is: ${counter}`);
+
 })
 
 //TEST funzione random
