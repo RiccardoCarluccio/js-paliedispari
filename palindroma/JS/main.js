d93a9c6a33fa9b75614e251e8c94280bc10e5f22
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-07-10 17:52:44 +0200

    Es#2 Input funzionante

---
+++ palindroma/JS/main.js
@@ -0,0 +1,9 @@
+const BTN_INPUT = document.querySelector(".btn-input");
+const USER_INPUT = document.getElementById("user-input");
+
+BTN_INPUT.addEventListener('click', function getWord() {
+  const USER_WORD = USER_INPUT.value;
+
+  //TEST INPUT
+  console.log(`The word is: ${USER_WORD}`);
+})
